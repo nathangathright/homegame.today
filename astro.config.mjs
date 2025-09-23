d16cd98f3da283390dc860410f1a584daf6d3fcb
Nathan Gathright <nathan@linktr.ee>
2025-09-23 13:00:41 -0500

    Add bluesky links

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   devToolbar: {
     enabled: false
   },
+  integrations: [icon()],
   site: "https://homegame.today",
   vite: {
     plugins: [tailwindcss()],
